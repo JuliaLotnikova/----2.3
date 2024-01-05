e19b06b440ccc09a22bfc656fc0ecf9c1f16c1e0
JuliaLotnikova <150443868+JuliaLotnikova@users.noreply.github.com>
2024-01-05 19:08:52 +0300

    v1

---
+++ script.js
@@ -0,0 +1,53 @@
+let a, b, result;
+
+// Присваивание
+a = 5;
+b = 2;
+
+// Сложение
+result = a + b;
+console.log(`Sum: ${result}`);
+
+// Вычитание
+result = a - b;
+console.log(`Difference: ${result}`);
+
+// Умножение
+result = a * b;
+console.log(`Multiplication: ${result}`);
+
+// Деление
+result = a / b;
+console.log(`Division: ${result}`);
+
+// Взятие остатка от деления
+result = a % b;
+console.log(`Remainder of the division: ${result}`);
+
+// Возведение в степень
+result = a ** b;
+console.log(`Exponentiation: ${result}`);
+
+// Оператор ">"
+result = a > b;
+console.log(`5 > 2: ${result}`);
+
+// Оператор "<"
+result = a < b;
+console.log(`5 < 2: ${result}`);
+
+// Оператор ">="
+result = a >= b;
+console.log(`5 >= 2: ${result}`);
+
+// Оператор "<="
+result = a <= b;
+console.log(`5 <= 2: ${result}`);
+
+// Оператор "===" (строгое равенство)
+result = a === b;
+console.log(`5 === 2: ${result}`);
+
+// Оператор "!==" (строгое неравенство)
+result = a !== b;
+console.log(`5 !== 2: ${result}`);
